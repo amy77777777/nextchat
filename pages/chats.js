3fc2c3bb427d0fbae959a7e8ccdaaf358e9b9587
Amit Udiwal <amit@shamlatech.com>
2022-12-31 14:47:09 +0530

    initial commit

---
+++ pages/chats.js
@@ -0,0 +1,49 @@
+import React,{useState,useEffect,useContext} from "react";
+
+import {Context} from '../context';
+
+import { useRouter } from "next/router";
+
+import dynamic from 'next/dynamic';
+
+const ChatEngine=dynamic(()=>
+import ("react-chat-engine").then((module)=>module.ChatEngine)
+);
+
+const MessageFormSocial=dynamic(()=>
+import ("react-chat-engine").then((module)=>module.MessageFormSocial)
+);
+
+
+
+export default function Chats() {
+
+  const {username,secret}=useContext(Context);
+  const [showChat,setShowChat]=useState(false);
+  const router=useRouter()
+
+  useEffect(()=>{
+    if(typeof document !==null){
+      setShowChat(true);
+    }
+  })
+
+  useEffect(()=>{
+    if(username.length === 0 || secret.length === 0 ) router.push("/")
+  })
+
+
+    if(!showChat) return <div />;
+
+  return <div className="background">
+    <div className="shadow">
+      <ChatEngine
+      height="calc(100vh-280px)"
+      projectID='ad01b94d-bd88-44e1-a900-ab54a14e30bd'
+      userName={username}
+      userSecret={secret}
+      renderNewMessageForm={()=><MessageFormSocial/>}
+      />
+    </div>
+  </div>;
+}
---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import React,{useContext} from "react";
+
+import {Context} from '../context';
+
+import  {useRouter} from 'next/router';
+
+import axios from 'axios';
+
+export default function Auth() {
+  // https://api.chatengine.io/users/
+
+    const {setUsername,username,secret,setSecret}=useContext(Context);
+    const router=useRouter();
+    // b984e024-d87e-40ab-b97b-6d429760d97d
+
+    function onSubmit(e){
+      e.preventDefault()
+
+        if(setUsername.length === 0 || setSecret.length === 0) return
+
+        axios.put(' https://api.chatengine.io/users/',
+        {username,secret},
+        {headers:{"Private-key":"b984e024-d87e-40ab-b97b-6d429760d97d"}}
+        ).then(r=>router.push('/chats'))
+
+
+    }
+
+
+  return <div className="background">
+    <div className="auth-container">
+      <form  className="auth-form" onSubmit={(e)=>onSubmit(e)}>
+<div className="auth-title">
+  NextJS Chat
+</div>
+<div className="input-container">
+  <input
+  placeholder="Email"
+  className="text-input"
+  onChange={e=>setUsername(e.target.value)}
+  >
+  
+  </input>
+</div>
+<div className="input-container">
+  <input
+  type="password"
+  placeholder="Password"
+  className="text-input"
+  onChange={e=>setSecret(e.target.value)}
+  >
+  
+  </input>
+</div>
+
+  <button
+  type="submit"
+  className="submit-button"
+  >Login/SignUp</button>
+      </form>
+    </div>
+  </div>;
+}
